cf1b4be43b77f3e67c440f08523e3962ad97b642
gejutaixiao <alshiwei@163.com>
2020-10-14 14:53:04 +0800

    网络请求

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,4 +1,6 @@
 // pages/home/home.js
+import request from '../../service/network.js'
+
 Page({
 
   /**
@@ -12,7 +14,48 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    // 发送网络请求
+    // 1.发送最简单的get请求
+    // wx.request({
+    //   url: 'http://123.207.32.32:8000/recommend',
+    //   success(res) {
+    //     console.log(res)
+    //   }
+    // })
 
+    // 2.get请求，并且携带参数
+    // wx.request({
+    //   url: 'http://123.207.32.32:8000/home/data',
+    //   data: {
+    //     type: 'sell',
+    //     page: 1
+    //   },
+    //   success(res) {
+    //     console.log(res)
+    //   }
+    // })
+
+    // 3.post请求，并且携带参数
+    // wx.request({
+    //   url: 'http://httpbin.org/post',
+    //   method: 'post',
+    //   data: {
+    //     name: 'coderwhy',
+    //     age: 18
+    //   },
+    //   success(res) {
+    //     console.log(res)
+    //   }
+    // })
+
+    // 2.使用封装的request发送网络请求
+    request({
+      url: 'http://123.207.32.32:8000/recommend'
+    }).then(res => {
+      console.log(res);
+    }).catch(err => {
+      console.log(err);
+    })
   },
 
   /**
