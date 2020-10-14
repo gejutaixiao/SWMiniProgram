d1c940f87e7ea7c6c324b868e1cad646f706f907
gejutaixiao <alshiwei@163.com>
2020-10-14 16:10:58 +0800

    登录流程和演练

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,58 @@
 // pages/home/home.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
+  handleShowToast() {
+    wx.showToast({
+      title: '加载中ing',
+      duration: 3000,
+      icon: 'loading',
+      mask: true
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  handleShowModal() {
+    wx.showModal({
+      title: '我是标题',
+      content: '我是内容',
+      // showCancel: false,
+      cancelText: '退出',
+      success: function(res) {
+        console.log(res);
+        if(res.cancel) {
+          console.log('用户点击了取消按钮')
+        }
+        if(res.confirm) {
+          console.log('用户点击了确定按钮')
+        }
+      }
+    })
   },
+  handleShowLoading() {
+    wx.showLoading({
+      title: '加载ing',
+      mask: true
+    })
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+    setTimeout(() => {
+      // 必须手动调用hideLoading才会使Loading消失
+      wx.hideLoading({
+        success: (res) => {},
+      })
+    }, 1000);
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  handleShowAction() {
+    wx.showActionSheet({
+      itemList: ['相册', '拍照'],
+      itemColor: 'red',
+      success: function(res) {
+        console.log(res);
+        
+      }
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  onShareAppMessage(options) {
+    return {
+      title: '你好啊李银河',
+      path: '/pages/about/about.wxml',
+      imageUrl: '/assets/cache.png'
+    }
   }
 })
